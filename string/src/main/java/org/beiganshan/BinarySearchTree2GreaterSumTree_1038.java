45680ccc492f5e5c172d789838f7b55b464f1770
Horatio <alexanderchen1992@gmail.com>
2021-05-01 12:31:50 +0800

    add explain

--- string/src/main/java/org/beiganshan/BinarySearchTree2GreaterSumTree_1038.java
+++ string/src/main/java/org/beiganshan/BinarySearchTree2GreaterSumTree_1038.java
@@ -1,5 +1,18 @@
 package org.beiganshan;
+/*
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
 
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class BinarySearchTree2GreaterSumTree_1038 {
     int sum = 0;
     public TreeNode bstToGst(TreeNode root) {
--- string/src/main/java/org/beiganshan/BrickWall_554.java
+++ string/src/main/java/org/beiganshan/BrickWall_554.java
@@ -2,7 +2,19 @@ package org.beiganshan;
 
 import java.util.HashMap;
 import java.util.List;
+/*
+There is a rectangular brick wall in front of you with n rows of bricks. The ith row has some number of bricks each of the same height (i.e., one unit) but they can be of different widths. The total width of each row is the same.
 
+Draw a vertical line from the top to the bottom and cross the least bricks. If your line goes through the edge of a brick, then the brick is not considered as crossed. You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+
+Given the 2D array wall that contains the information about the wall, return the minimum number of crossed bricks after drawing such a vertical line.
+
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/brick-wall
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class BrickWall_554 {
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer, Integer> map = new HashMap<>();
